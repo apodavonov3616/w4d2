20006ce94090556855812a719dc2453d773486b9
apodavonov3616 <apodavonov@gmail.com>
2022-04-19 15:23:45 -0400

    making board

--- chess/Board.rb
+++ chess/Board.rb
@@ -4,6 +4,14 @@ class Board
 
     def initialize
         @grid = Array.new(8) { Array.new(8, nil) }
+        Board.populate
+    end
+
+    def self.populate
+        array = [ :R, :N, :B, :Q, :K, :B, :N, :R]
+        (0...8).each do |piece|
+            ele.new()
+        end
     end
 
     def [](pos)
@@ -14,6 +22,13 @@ class Board
     def []=(pos,val)
         row, col = pos
         @grid[row][col] = val
+    end
+
+    def move_piece(start_pos, end_pos)
+        raise 'no piece at starting position' if self[start_pos] == nil
+        raise 'cannot move to end_pos' if #how to find out if piece can move there
+        self[start_pos] = nil
+        self[end_pos] = #how to get access to piece to move it
     end
 end
 
